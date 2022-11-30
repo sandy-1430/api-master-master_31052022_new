237f9661c64cf075d04d56eaa791b2041c79e505
sandy-1430 <73515551+sandy-1430@users.noreply.github.com>
2022-11-30 11:11:46 +0530

    payment

--- src/Pages/Payment/pay.js
+++ src/Pages/Payment/pay.js
@@ -34,6 +34,7 @@ const paymentHandler = ({ couponCode, profileData, totalPrice, setLoader, checko
         orderAmt: totalPrice,
       }
     }
+
     axios
       .post(
         baseUrl() + "/pg/initiatePayment", obj,
